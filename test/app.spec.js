d9e39cad0f7a3c7a5c3910c90ac93c726ba1a5b3
Muhet <muhedarius96@gmail.com>
2023-03-15 13:47:18 +0200

    new integration yml file added

--- test/app.spec.js
+++ test/app.spec.js
@@ -8,7 +8,37 @@ chai.should();
 
 describe("Blog API", () => {
 
+  describe("POST /api/blogs/create", () => {
+    it("should create a new project", (done) => {
+      const newBlog = {
+        title: "Test Title",
+        category: "Test Category",
+        image: "test-image.png",
+        description: "Test description2",
+      };
+      chai
+        .request(app)
+        .post("/api/blogs/create")
+        .send(newBlog)
+        .end((err, res) => {
+           
+         expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+          expect(res.body.data[0]).to.have.property('title').eql('Test Title');
+          expect(res.body.data[0]).to.have.property('category').eql('Test Category');
+          expect(res.body.data[0]).to.have.property('image').eql('test-image.png');
+          expect(res.body.data[0]).to.have.property('description').eql('Test description');
+          newProject._id = res.body.data[0]._id;
+          console.log('########################################################')
+          console.log(res.status)
+          console.log('########################################################')
+           //*  */
+        
+      });
+      done();
+  });  
 
+  });
 
   // Test the POST /api/project/create route
   describe("POST /api/project/create", () => {
